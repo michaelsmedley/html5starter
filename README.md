63432973e5c1c5cfb86ebbe52f7dff52e98122f9
michaelsmedley <mike@michael-smedley.co.uk>
2013-10-10 06:57:37 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+A basic starter kit for a HTML5 website
+
+More details coming soon.
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php require_once(dirname(__FILE__).'/inc/header.php'); ?>
+
+        <!-- Add your site or application content here -->
+        <p>Hello world! This is HTML5 Boilerplate.</p>
+        
+<?php require_once(dirname(__FILE__).'/inc/footer.php'); ?>
